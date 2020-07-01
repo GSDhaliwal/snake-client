3672e775df9094051c97499d9d2d5188f49f147e
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-01 19:30:06 +0000

    exported connect() from play.js to client.js

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  //event handler, to handle incoming data from server. prints messge for player
-  conn.on('data', (data) => {
-    console.log(data)
-  })
-
-  return conn;
-}
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
