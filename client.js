4ae654ffaa613b6e93bf26e3a7607f089843fceb
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-01 20:04:27 +0000

    got signal to show connection to server, and sent server a message with 'Name:___'

--- client.js
+++ client.js
@@ -5,6 +5,14 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+
+  //'connect' event is triggered as soon as connection with server is established
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    //sending message to ther server
+    conn.write('Name: GSD');
+  })
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
@@ -17,3 +25,5 @@ const connect = function() {
 }
 
 module.exports = connect;
+
+//To set the name, we simply have to send the server a message in the form: Name: SNK where SNK can be any three alpha-numeric characters. The name is then shown to everyone on the game server.
